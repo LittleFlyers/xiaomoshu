3806107a6d8a84968a11bd49cf9d1ea46184f1e7
LittleFlyers <2538285284@qq.com>
2023-01-10 21:47:28 +0800

    modify ButtonEditText

--- client/Android/Xiaomoushu/weights/src/main/java/com/zpf/xiaomoushu/weights/ButtonEditText.kt
+++ client/Android/Xiaomoushu/weights/src/main/java/com/zpf/xiaomoushu/weights/ButtonEditText.kt
@@ -1,12 +1,12 @@
 package com.zpf.xiaomoushu.weights
 
-import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Rect
+import android.text.InputType
 import android.util.AttributeSet
 import android.view.MotionEvent
 
@@ -14,15 +14,12 @@ import android.view.MotionEvent
 class ButtonEditText : BaseEditText {
     companion object {
         private const val TAG = "ButtonEditText"
-
-        private const val ANIMATOR_TIME = 200
     }
 
     private lateinit var mButtonBitmap: Bitmap
-    private lateinit var mVisibleAnimator: ValueAnimator
-    private var mIsPassword: Boolean = false
     private var mBtnWidth = 0
     private var mBtnPadding = 0
+    private var mIsPassword: Boolean = false
     private var mIsButtonVisible = false
 
     constructor(context: Context) : super(context) {
@@ -54,21 +51,19 @@ class ButtonEditText : BaseEditText {
             typeArray.recycle()
         }
 
-        mButtonBitmap = if (mIsPassword) {
-            BitmapFactory.decodeResource(context.resources, R.mipmap.password)
-        } else {
-            BitmapFactory.decodeResource(context.resources, R.mipmap.clear)
+        if (!mIsPassword) {
+            mButtonBitmap = BitmapFactory.decodeResource(context.resources, R.mipmap.clear)
         }
 
         mBtnWidth = context.resources.getDimensionPixelSize(R.dimen.btn_edit_text_width)
         mBtnPadding = context.resources.getDimensionPixelSize(R.dimen.btn_edit_text_padding)
-
-        mVisibleAnimator = ValueAnimator.ofFloat(0f, 1f).setDuration(ANIMATOR_TIME.toLong())
-
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        if (mIsPassword) {
+            inputType = (InputType.TYPE_TEXT_VARIATION_PASSWORD or InputType.TYPE_CLASS_TEXT)
+        }
         drawButton(canvas)
     }
 
@@ -89,22 +84,19 @@ class ButtonEditText : BaseEditText {
                     && event.x < width - mBtnPadding
                     && isFocused)
             if (buttonTouched) {
-
-
-                if (mIsPassword) {
-
-                } else {
-                    setText("")
-                    return true
-                }
+                onClearButtonClick()
+                return true
             }
-
         }
         return super.onTouchEvent(event)
     }
 
+    private fun onClearButtonClick() {
+        setText("")
+    }
+
     private fun drawButton(canvas: Canvas) {
-        if (mIsButtonVisible) {
+        if (mIsButtonVisible && !mIsPassword) {
             drawClearButton(canvas)
             invalidate()
         }
